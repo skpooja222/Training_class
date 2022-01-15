6c96e8e5ed9883c2f2825c20fc6882b4c24f34ce
skpooja222 <poojakumbar819@gmail.com>
2022-01-15 20:34:56 +0530

    added code

---
+++ C++_training/Pure_virtual_function/Pure_virtual_function.cpp
@@ -0,0 +1,34 @@
+// C++ program to calculate the area of a square and a circle
+
+#include <iostream>
+using namespace std;
+
+// Abstract class
+class Shape {
+   protected:
+    float dimension;
+
+   public:
+    void getDimension() {
+        cin >> dimension;
+    }
+
+    // pure virtual Function
+    virtual float calculateArea() = 0;
+};
+
+// Derived class
+class Square : public Shape {
+   public:
+    float calculateArea() {
+        return dimension * dimension;
+    }
+};
+
+// Derived class
+class Circle : public Shape {
+   public:
+    float calculateArea() {
+        return 3.14 * dimension * dimension;
+    }
+};
